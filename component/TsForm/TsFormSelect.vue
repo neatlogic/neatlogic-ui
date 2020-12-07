1ae2e615515bf860819dc118be9ae82baeb31ef4
zhuangqp <zhuangqp@techsure.com.cn>
2020-12-07 16:20:42 +0800

    修复下拉0的回显

--- component/TsForm/TsFormSelect.vue
+++ component/TsForm/TsFormSelect.vue
@@ -277,7 +277,7 @@ export default {
         _this.nodeList = [];
         let ajaxArr = { method: _this.ajaxType, url: _this.url};
         let needdataLi = ['post', 'put'];
-        needdataLi.indexOf(_this.ajaxType) < 0 ? Object.assign(ajaxArr, {params: params}) : Object.assign(ajaxArr, {data: params});
+        ArrIndexOf(needdataLi, _this.ajaxType) < 0 ? Object.assign(ajaxArr, { params: params }) : Object.assign(ajaxArr, { data: params });
         axios(ajaxArr).then(res => {
           if (res && res.Status == 'OK') {
             _this.nodeList = _this.setNodeList(res.Return);
@@ -323,7 +323,7 @@ export default {
       }
       if (ary.length > 0) {
         _this.nodeList.forEach(function(item) {
-          ary.indexOf(item[_this.valueName]) >= 0 && _this.selectedList.push(item);
+          ArrIndexOf(ary, item[_this.valueName]) >= 0 && _this.selectedList.push(item);
         });
       } else if (_this.defaultValueIsFirst && ary.length <= 0 && this.isRequired && _this.nodeList.length == 1) { //如果必填，而且下拉值只有一个则默认选中第一个
         _this.selectedList.push(_this.nodeList[0]);
@@ -365,7 +365,7 @@ export default {
         return;
       }
 
-      if ((_this.currentValue instanceof Array && _this.currentValue.indexOf(query) > 0) || _this.currentValue == query) {
+      if ((_this.currentValue instanceof Array && ArrIndexOf(_this.currentValue, query) > 0) || _this.currentValue == query) {
         return;
       }
       this.dynamicSearch(query);
@@ -409,7 +409,7 @@ export default {
     },
     deleteSeleted(ind, value, list) {
       list.splice(ind, 1);
-      this.currentValue.splice(this.currentValue.indexOf(value), 1);
+      this.currentValue.splice(ArrIndexOf(this.currentValue, value), 1);
       this.$refs.input && this.$refs.input.focus();
       this.onChangeValue();
     },
@@ -457,7 +457,7 @@ export default {
       typeof _this.params == 'object' && Object.assign(params, _this.params);
       let ajaxArr = { method: _this.ajaxType, url: _this.dynamicUrl};
       let needdataLi = ['post', 'put'];
-      needdataLi.indexOf(_this.ajaxType) < 0 ? Object.assign(ajaxArr, {params: params}) : Object.assign(ajaxArr, {data: params});
+      ArrIndexOf(needdataLi, _this.ajaxType) < 0 ? Object.assign(ajaxArr, { params: params }) : Object.assign(ajaxArr, { data: params });
       axios(ajaxArr).then(res => {  
         if (res && res.Status == 'OK') {
           //初始化数据时需要text值
@@ -497,7 +497,7 @@ export default {
       typeof _this.params == 'object' && Object.assign(params, _this.params);
       let ajaxArr = {method: _this.ajaxType, url: _this.dynamicUrl, cancelToken: _this.cancelAxios.token };
       let needdataLi = ['post', 'put'];
-      needdataLi.indexOf(_this.ajaxType) < 0 ? Object.assign(ajaxArr, {params: params}) : Object.assign(ajaxArr, {data: params});
+      ArrIndexOf(needdataLi, _this.ajaxType) < 0 ? Object.assign(ajaxArr, { params: params }) : Object.assign(ajaxArr, { data: params });
       axios(ajaxArr).then(res => {
         _this.loading = false;
         if (res && res.Status == 'OK') {
@@ -602,8 +602,8 @@ export default {
       if (selectli && selectli.length > 0) {
         if (this.multiple) {
           //多选
-          if (this.currentValue.indexOf(value) > -1) {
-            this.currentValue.splice(this.currentValue.indexOf(value), 1);
+          if (ArrIndexOf(this.currentValue, value) > -1) {
+            this.currentValue.splice(ArrIndexOf(this.currentValue, value), 1);
             selectli.forEach((se, ind) => {
               if (se[this.valueName] == value) {
                 selectli.splice(ind, 1);
@@ -729,7 +729,7 @@ export default {
           let keyval = _this.addItem ? _this.addItem[_this.textName] || this.searchKeyWord : this.searchKeyWord;
           if (keyval && _this.nodeList && _this.nodeList.length > 0) {
             _this.nodeList.find(no => {
-              if (no[_this.valueName] === keyval || no[_this.textName] === keyval) {
+              if (this.$utils.equalStr(no[_this.valueName], keyval) || this.$utils.equalStr(no[_this.textName], keyval)) {
                 isExist = true;
               }
               return isExist;
@@ -751,7 +751,7 @@ export default {
           if (this.multiple && !this.searchKeyWord && this.selectedList.length > 0) {
             let lastLi = this.selectedList.length - 1;
             let lastval = this.selectedList[lastLi][this.valueName];
-            this.currentValue.splice(this.currentValue.indexOf(lastval), 1);
+            this.currentValue.splice(ArrIndexOf(this.currentValue, lastval), 1);
             this.selectedList.splice(lastLi, 1);
             this.onChangeValue();
           }
@@ -851,9 +851,9 @@ export default {
       return function(node, index) {
         let _this = this;
         let classtxt = 'select-li ivu-dropdown-item overflow';
-        if (this.multiple && this.currentValue && this.currentValue.indexOf(node[_this.valueName]) > -1) {
+        if (this.multiple && this.currentValue && ArrIndexOf(this.currentValue, node[_this.valueName]) > -1) {
           classtxt = classtxt + ' selected';
-        } else if (!this.multiple && this.currentValue === node[_this.valueName]) {
+        } else if (!this.multiple && this.$utils.equalStr(this.currentValue, node[_this.valueName])) {
           classtxt = classtxt + ' selected';
         }
         if (index + 1 == _this.focusIndex || node['_focusSelect']) {
@@ -972,7 +972,7 @@ export default {
       } else if (!val && !this.multiple) {
         //当收起下拉框时，是单选
         if (this.selectedList[0] && !this.allowCreate && typeof this.$listeners['enter-search'] != 'function') {
-          if (this.searchKeyWord === this.selectedList[0][this.textName] || this.searchKeyWord === this.selectedList[0][this.valueName]) {
+          if (this.$utils.equalStr(this.searchKeyWord, this.selectedList[0][this.textName]) || this.$utils.equalStr(this.searchKeyWord, this.selectedList[0][this.valueName])) {
             this.searchKeyWord = this.selectedList[0] ? this.selectedList[0][this.textName] : '';
           }
         }
@@ -1007,6 +1007,24 @@ function setWidth($contain, $target, transfer) {
     // if (transfer) {
     //   $target.parentNode.style.width = 'auto';
     // }
+  }
+}
+function ArrIndexOf(arr, str) {
+  //重写 数组的indexof功能
+  if (arr instanceof Array) {
+    let index = -1;
+    let has = arr.find((item, i) => {
+      if (item == str) {
+        index = i;
+        return true;
+      }
+      return false;
+    });
+    return index;
+  } else if (typeof arr == 'string') {
+    return arr.indexOf(str);
+  } else {
+    return -1;
   }
 }
 </script>
--- static/js/util.js
+++ static/js/util.js
@@ -1,12 +1,17 @@
 /*
-getCookie(name)                 获取cookie为name的值
-setCookie(name, value, time)    设置cookie为name的值为value，期限是time(如果是数字，单位为天；如果是字符串，直接赋值结束时间)
-removeCookie(name)              清除cookie为name的值
-deepClone(data)                 复制数据（不改动原数据）
-setUuid()                       生成32位随机uuid
-getCurrenttime(format)          根据指定格式格式化当前时间输出格式
-sortNumber()                    数字排序
- */
+getCookie(name)                              获取cookie为name的值
+setCookie(name, value, time)                 设置cookie为name的值为value，期限是time(如果是数字，单位为天；如果是字符串，直接赋值结束时间)
+removeCookie(name)                           清除cookie为name的值
+deepClone(data)                              复制数据（不改动原数据）
+setUuid()                                    生成32位随机uuid
+getCurrenttime(format)                       根据指定格式格式化当前时间输出格式
+sortNumber()                                 数字排序
+getDateByFormat(value, format, styleType)    通过时间戳获取指定格式的日期
+toggleClass(el, classname, type)             修改class 1、el:目标元素  2、cls:需要操作的class  3、type:"add"代表添加对应的class  "remove"代码删除对应的class 如果不传则有则删除，没有则添加
+getValueByPath(value, path, isArrary)        值的为空判断 如果是数组，数组的长度判断 //1、value：[Nothing]需要做判断的值, 2、path：[String]需要层层遍历的  3、isArrary:[Boolean]最终获取的值是数组，而且数组的长度大于0
+checkType(value, type)                       检查value类型，如果type为空着放回值的类型
+validForm($el,$scroll)                       整体表单控件校验,并且滚动到报错地方 可以传dom对象，也可以传选择器字符串
+*/
 export default {
   getCookie: function(name) {
     if (name) {
@@ -65,7 +70,7 @@ export default {
     });
   },
   getCurrenttime(format) {
-    //参数默认格式：年月日 时:分:秒，其他选项：["HH:mm:ss","yyyy-MM-dd","HH:mm")]
+    // 参数默认格式：年月日 时:分:秒，其他选项：["HH:mm:ss","yyyy-MM-dd","HH:mm")]
     let date = new Date();
     let year = date.getFullYear();
     let month = date.getMonth() + 1;
@@ -93,7 +98,7 @@ export default {
     return timestr;
   },
   getDateByFormat(value, format, styleType) {
-    //时间戳，转成字符串
+    // 时间戳，转成字符串
     value = new Date(value);
     styleType = styleType || '-';
     format = format || 'yyyy-MM-dd HH:mm:ss';
@@ -139,7 +144,134 @@ export default {
     };
   },
   concatArr(a, ...arr) {
-    //合并多个数组且去重
+    // 合并多个数组且去重
     return Array.from(new Set(a.concat(...arr)));
+  },
+  validForm($el, $scroll) {
+    //校验表单插件 dom对象，选择器字符串
+    let isVaild = true;
+    let $error = null;
+    if ($el) {
+      //进行校验
+      let childrendom = [];
+      if (this.checkType($el, 'string')) {
+        childrendom = document.querySelectorAll($el + ' .form-li');
+      } else {
+        childrendom = $el.querySelectorAll('.form-li');
+      }
+
+      if (childrendom && childrendom.length > 0) {
+        childrendom.forEach(children => {
+          if (children.__vue__ && children.__vue__.valid) {
+            if (!children.__vue__.valid()) {
+              isVaild && ($error = children);
+              isVaild = false;
+            }
+          }
+        });
+      }
+    }
+    if (!isVaild && $scroll) {
+      //如果校验不通过滚动到具体的位置
+      this.checkType($scroll, 'string') && ($scroll = document.querySelector($scroll));
+      let targetConfig = $error.getBoundingClientRect();
+      let containConfig = $scroll.getBoundingClientRect();
+      if (!(targetConfig.top > containConfig.top && targetConfig.top < containConfig.top + containConfig.height)) {
+        $scroll.scrollTop = Math.abs(targetConfig.top - containConfig.top) + 50;
+      }
+    }
+    return isVaild;
+  },
+  toggleClass(el, cls, type) {
+    // 1、el:目标元素  2、cls:需要操作的class  3、type:"add"代表添加对应的class  "remove"代码删除对应的class 如果不传则有则删除，没有则添加
+    if (!el || !cls) {
+      return;
+    }
+    cls = cls.trim().split(' '); // 需要处理的class数组
+    let curClass = el.className ? el.className.trim().split(' ') : []; // 现有的class数组
+
+    for (let i = 0, j = cls.length; i < j; i++) {
+      const clsName = cls[i];
+      if (!clsName.trim()) {
+        continue;
+      }
+      let index = curClass.indexOf(clsName);
+      if (index >= 0 && type !== 'add') {
+        curClass.splice(index, 1);
+      } else if (index < 0 && type !== 'remove') {
+        curClass.push(clsName);
+      }
+    }
+    el.className = curClass.join(' ');
+  },
+  getValueByPath(value, path, isArrary) {
+    //1、value：[Nothing]需要做判断的值, 2、path：[Array]需要层层遍历的  3、isArrary:[Boolean]最终获取的值是数组，而且数组的长度大于0
+    let result = value;
+    if (value === null || value === undefined) {
+      result = false;
+    } else if (path && !value) {
+      result = false;
+    } else if (typeof value == 'object' && path) {
+      let pathList = path.split('.');
+      let val = value;
+      pathList &&
+        pathList.forEach((item, i) => {
+          val !== false && (val = this.getDeepData(val[item]));
+        });
+      result = val;
+    } else if (path && typeof value !== 'object') {
+      result = false;
+    }
+    if (!(result && isArrary && result instanceof Array && result.length > 0)) {
+      result = false;
+    }
+    return result;
+  },
+  checkType(value, type) {
+    // 检查数据类型 1、value:[nothing]需要检查的数据 2、type :[string]类型,如果type不存在直接返回类型 null undefined number boolean string object array set map function  date regexp  promise symbol error
+    let result = false;
+    let str = Object.prototype.toString.call(value).slice(8, -1);
+    if (!type) {
+      //如果type不存在则放回类型
+      return str;
+    }
+    if (typeof type == 'string') {
+      if (str.toUpperCase() == type.toUpperCase()) {
+        result = true;
+      }
+    } else if (type instanceof Array) {
+      type.find(item => {
+        if (str.toUpperCase() == item.toUpperCase()) {
+          result = true;
+          return true;
+        }
+        return false;
+      });
+    }
+    return result;
+  },
+  checkStringNotEmpty(value) {
+    // 检查数据类型 1、value:[nothing]需要检查的数据 2、type :[string]类型,如果type不存在直接返回类型 null undefined number boolean string object array set map function  date regexp  promise symbol error
+    return !this.checkType(value, ['null', 'undefined']) && value !== '';
+  },
+  equalStr(str1, str2) {
+    //判断两个字符串是否是相同 主要是为了对 0 和 '0'需要判断相同的情况下面
+    if (this.checkType(str1, 'null') || this.checkType(str1, 'undefined')) {
+      //当两个值有
+      if (this.checkType(str2, 'null') || this.checkType(str2, 'undefined')) {
+        return true;
+      }
+    } else if (str1 === 0) {
+      if ((this.checkType(str2, 'number') && str2 === 0) || (this.checkType(str2, 'string') && str2 === '0')) {
+        return true;
+      }
+    } else if (str2 === 0) {
+      if ((this.checkType(str1, 'number') && str1 === 0) || (this.checkType(str1, 'string') && str1 === '0')) {
+        return true;
+      }
+    } else {
+      return str1 == str2;
+    }
+    return false;
   }
 };
