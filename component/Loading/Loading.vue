60672ad117620241c4bfdcff4b29cf92b45e5d6a
zhuangqp <zhuangqp@techsure.com.cn>
2020-12-23 19:11:30 +0800

    add

--- component/Loading/Loading.vue
+++ component/Loading/Loading.vue
@@ -1,20 +1,11 @@
 <template>
-  <div>
-    <div v-if="type == 'fix'">
-      <Spin v-if="show" size="large" fix class="text-primary">
-        <Icon type="ios-loading" size="18" class="loading"></Icon>
-        <div>加载中</div>
-      </Spin>
-    </div>
-    <div v-else class="part-loading">
-      <Spin v-if="show" size="large" class="text-primary">
-        <Icon type="ios-loading" size="18" class="loading"></Icon>
-        <div>加载中</div>
-      </Spin>
-    </div>
+  <div :class="type == 'fix'?'':'part-loading'">
+    <Spin v-if="loadingShow" size="large" :fix="type == 'fix'?true:false" class="text-primary">
+      <Icon type="ios-loading" size="18" class="loading"></Icon>
+      <div>加载中</div>
+    </Spin>
   </div>
 </template>
-
 <script>
 export default {
   name: 'Loading',
@@ -31,25 +22,14 @@ export default {
   },
   data() {
     return {
-      show: this.loadingShow,
-      loadingtype: this.type
     };
   },
-
   created() {},
-
   beforeMount() {},
-
   mounted() {},
-
   methods: {},
-
   computed: {},
-
   watch: {
-    loadingShow: function() {
-      this.show = this.loadingShow;
-    }
   }
 };
 </script>
--- iview/utils/assist.js
+++ iview/utils/assist.js
@@ -181,12 +181,15 @@ function findComponentUpward(context, componentName, componentNames) {
   } else {
     componentNames = componentName;
   }
-
   let parent = context.$parent;
   let name = parent.$options.name;
   while (parent && (!name || componentNames.indexOf(name) < 0)) {
     parent = parent.$parent;
-    if (parent) { name = parent.$options.name; }
+    if (parent) { 
+      name = parent.$options.name; 
+    } else {
+      break;
+    }
   }
   return parent;
 }
