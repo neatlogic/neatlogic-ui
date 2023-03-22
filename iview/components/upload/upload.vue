e4ce3cc57db8e78567cfea798e571f85abf2fe1e
dengbf <dengbf@aliyun.com>
2023-03-22 12:04:24 +0800

    commit

--- iview/components/upload/upload.vue
+++ iview/components/upload/upload.vue
@@ -300,12 +300,11 @@ export default {
     handleError(err, response, file) {
       const _file = this.getFile(file);
       const fileList = this.fileList;
-
-      _file.status = 'fail';
-
-      fileList.splice(fileList.indexOf(_file), 1);
-
-      this.onError(err, response, file);
+      if (_file) {
+         _file.status = 'fail';
+        fileList.splice(fileList.indexOf(_file), 1);
+        this.onError(err, response, file);
+      } 
     },
     handleRemove(file) {
       const fileList = this.fileList;
